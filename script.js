bb1c58dd6133f2126f9e61ee9dad71016fd959ae
unknown <victordalet@protonmail.com>
2023-01-19 15:24:02 +0100

    machine learning

--- script.js
+++ script.js
@@ -13,8 +13,8 @@ function forward_propagation(X, parametres) {
     let activations = {'A0':X};
     let C = Object.keys(parametres).length / 2;
     for (let c = 1; c <= C; c++) {
-        let Z = math.add(math.multiply(parametres['W' + c], activations['A' + (c-1)]), parametres['b' + c]);
-        activations['A' + c] = math.divide(1, math.add(1, math.exp(math.unaryMinus(Z))));
+        let Z = (parametres['W' + c] * activations['A' + (c-1)]) + parametres['b' + c];
+        activations['A' + c] = 1 / (1 + Math.exp(-Z));
     }
     return activations;
 }
@@ -25,10 +25,10 @@ function back_propagation(y, parametres, activations) {
     let dZ = math.subtract(activations['A' + C], y);
     let gradients = {};
     for (let c = C; c >= 1; c--) {
-        gradients['dW' + c] = math.divide(math.multiply(dZ, math.transpose(activations['A' + (c - 1)])), m);
-        gradients['db' + c] = math.divide(math.sum(dZ, 1), m);
+        gradients['dW' + c] = (dZ * math.dot(dZ, math.transpose(activations['A' + (c - 1)])) / m);
+        gradients['db' + c] = (dZ + 1) / m;
         if (c > 1) {
-            dZ = math.multiply(math.multiply(math.dot(math.transpose(parametres['W' + c]), dZ), math.subtract(1, activations['A' + (c - 1)])), activations['A' + (c - 1)]);
+            dZ = ((math.dot(math.transpose(parametres['W' + c]), dZ) * math.subtract(1, activations['A' + (c - 1)])) * activations['A' + (c - 1)]);
         }
     }
     return gradients;
@@ -37,8 +37,8 @@ function back_propagation(y, parametres, activations) {
 function update(gradients, parametres, learning_rate) {
     let C = Object.keys(parametres).length / 2;
     for (let c = 1; c <= C; c++) {
-        parametres['W' + c] = math.subtract(parametres['W' + c], math.multiply(gradients['dW' + c], learning_rate));
-        parametres['b' + c] = math.subtract(parametres['b' + c], math.multiply(gradients['db' + c], learning_rate));
+        parametres['W' + c] = math.subtract(parametres['W' + c], (gradients['dW' + c] *  learning_rate));
+        parametres['b' + c] = math.subtract(parametres['b' + c], (gradients['db' + c] *  learning_rate));
     }
     return parametres;
 }
@@ -47,7 +47,7 @@ function predict(X, parametres) {
     let activations = forward_propagation(X, parametres);
     let C = Object.keys(parametres).length / 2;
     let Af = activations['A' + C];
-    return math.largerEq(Af, 0.5);
+    return Af >= 0,5;
 }
 
 function logLoss(yTrue, yPred) {
@@ -73,21 +73,20 @@ function accuracyScore(yTrue, yPred) {
 
 function deepNeuralNetwork(X, y, hiddenLayers = [16, 16, 16], learningRate = 0.001, nIter = 3000) {
     // Initialize parameters
-    var dimensions = hiddenLayers.slice();
-    dimensions.unshift(X.shape[0]);
-    dimensions.push(y.shape[0]);
-    Math.seedrandom(1);
+    var dimensions = [X.length].concat(hiddenLayers);
+    dimensions.unshift(2); // dimension of X
+    dimensions.push(1); // dimension of y
     var parameters = initialization(dimensions);
 
     // Create an array for future accuracy and log_loss
     var trainingHistory = new Array(nIter);
-
     var C = parameters.length / 2;
 
     // Gradient descent
     for (var i = 0; i < nIter; i++) {
         var activations = forward_propagation(X, parameters);
         var gradients = back_propagation(y, parameters, activations);
+        console.log(2);
         parameters = update(gradients, parameters, learningRate);
         var Af = activations['A' + C];
 
@@ -96,6 +95,5 @@ function deepNeuralNetwork(X, y, hiddenLayers = [16, 16, 16], learningRate = 0.0
         var yPred = predict(X, parameters);
         trainingHistory[i][1] = accuracyScore(y.flatten(), yPred.flatten());
     }
-
     return trainingHistory;
 }
